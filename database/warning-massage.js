2fd5dcafe9e9f5bdd697c79db89f93504129a744
poudel06 <santoshpoudel06.com.np@gmail.com>
2024-09-12 10:36:57 +0545

    biggest changes fixing
    i fix some error and unwanted things removed ect

--- database/warning-massage.js
+++ database/warning-massage.js
@@ -1,4 +1,4 @@
 console.log(
-    "%c\n\n जान्ने पल्टिएको होस् मूजि \n\n\n",
+    "%c\n\n जान्ने पल्टिएको होस्\n\n\n",
     "font-size: 20px; color:#F28123; padding: 10px; border-radius: 5px; text-align: center;"
 );
